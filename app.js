fd7eb1a53d07644c03c8cbe7a6dd44645216f531
RakibRahman <md.rr.talukder@gmail.com>
2021-03-25 21:27:13 +0600

    Paste functionality added

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-// const copyArea = document.getElementById("copyArea");
-// const pasteArea = document.getElementById("pasteArea");
-// const copyBTN = document.getElementById("copyBTN");
-// const pasteBTN = document.getElementById("pasteBTN");
 const confirm = document.getElementById("confirm");
 const reset = document.getElementById("reset");
 const support = document.getElementById("support");
@@ -12,12 +8,55 @@ reset.addEventListener("click", function reset() {
 
 // //! Detecting Support
 support.addEventListener("click", function () {
-  return navigator.clipboard
+  navigator.clipboard
     ? (confirm.innerHTML = "Clipboard Api is Supported")
     : (confirm.innerHTML = "Clipboard Api is Not Supported");
+  setTimeout(() => {
+    confirm.innerHTML = "";
+  }, 1000);
 });
-const readBtn = document.querySelector(".read-btn");
-const writeBtn = document.querySelector(".write-btn");
+//! copy the text
+const copyArea = document.getElementById("copyArea");
+const copyBTN = document.getElementById("copyBTN");
 
-const resultsEl = document.querySelector(".clipboard-results");
-const inputEl = document.querySelector(".to-copy");
+const copyText = () => {
+  const inputData = copyArea.value.trim(); //?which element to copy
+
+  if (inputData) {
+    navigator.clipboard
+      .writeText(inputData)
+      .then(() => {
+        inputData.value = "";
+
+        //! change content of copy btn on successful copy
+        if (copyBTN.innerText !== "copied") {
+          const originalText = copyBTN.innerText;
+          copyBTN.innerText = "Copied";
+          setTimeout(() => {
+            copyBTN.innerText = originalText;
+          }, 1000);
+        }
+      })
+      .catch((error) => {
+        console.log("something went wrong", error);
+      });
+  }
+};
+copyBTN.addEventListener("click", copyText);
+
+//! paste the text
+const pasteArea = document.getElementById("pasteArea");
+const pasteBTN = document.getElementById("pasteBTN");
+
+const pasteData = () => {
+  navigator.clipboard
+    .readText()
+    .then((text) => {
+      pasteArea.innerHTML = text;
+      //? `text` contains the text read from the clipboard
+    })
+    .catch((error) => {
+      console.log("Something went wrong", error);
+    });
+};
+pasteBTN.addEventListener("click", pasteData);
